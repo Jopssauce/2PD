178bb34bf36b85421ccbb84ec230377914663ac2
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-05-28 22:41:03 +0800

    Enemy now damages player

---
+++ 2PD/Assets/Scripts/Enemy/EnemyCollideAttack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class EnemyCollideAttack : MonoBehaviour {
+	public float attackTimer;
+	public float damage;
+	public EnemyAI enemyAI;
+
+
+	public virtual void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{	
+			if(enemyAI.players[0] == null) return;		
+			StartCoroutine(Attack());
+		}
+		
+	}
+	public virtual void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			StopCoroutine(Attack());
+		}
+	}
+
+	public IEnumerator Attack()
+	{
+		while (enemyAI.players[0] != null)
+		{
+			enemyAI.players[0].GetComponent<PlayerStats>().DeductHp(damage);
+			yield return new WaitForSeconds(attackTimer);
+		}
+		
+	}
+
+}
