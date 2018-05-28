8bebe3abf45c91820bd0afd92001adab4ee680e2
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-05-28 23:37:53 +0800

    Removed knockback from projectiles for now

--- 2PD/Assets/Scripts/Player/BulletBehaviour.cs
+++ 2PD/Assets/Scripts/Player/BulletBehaviour.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletBehaviour : MonoBehaviour {
-	
+	public float moveSpeed = 2;
+	public Vector3 direction;
 	// Update is called once per frame
 	void FixedUpdate () 
 	{
-		GetComponent<Rigidbody2D>().velocity = transform.up * 6;
+		direction.x = 0;
+		direction.y = 1;
+		//GetComponent<Rigidbody2D>().velocity = transform.up * 6;
+		this.transform.position += transform.up * moveSpeed * Time.deltaTime;
 		Destroy(this.gameObject, 2.0f);
 	}
-	void OnCollisionEnter2D(Collision2D col)
+	void OnTriggerEnter2D(Collider2D col)
     {
 		if (col.gameObject.GetComponent<PlayerStats>()) 
 		{
