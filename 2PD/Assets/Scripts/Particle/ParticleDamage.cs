3453ff75890d91270e1e560c3406a6799a2e242e
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-02 19:55:35 +0800

    Particle damage added to give fire hazard damage

---
+++ 2PD/Assets/Scripts/Particle/ParticleDamage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleDamage : MonoBehaviour {
+
+	 void OnParticleCollision(GameObject other)
+    {
+        Rigidbody2D body = other.GetComponent<Rigidbody2D>();
+		PlayerController player = other.GetComponent<PlayerController>();
+        if (body)
+        {
+            Vector3 direction = other.transform.position - transform.position;
+            direction = direction.normalized;
+            body.AddForce(Vector3.up * 5);
+        }
+		if (player)
+        {
+           player.GetComponent<PlayerStats>().DeductHp(4);
+        }
+    }
+}
