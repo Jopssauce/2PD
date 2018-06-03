fb98079645f201ba27a7082f54156b6edf2b7e07
Luis Gabriel D. Evaristo <>
2018-06-03 13:56:49 +0800

    added encyclopedia UI and base scripts, added a town scene still nothing in it but im having an issue with the camera thing

---
+++ 2PD/Assets/Encyclo/Script/EncyclopediaItems.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Item Description", menuName = "Gem Description")]
+public class EncyclopediaItems : ScriptableObject
+{
+	public string ItemName;
+
+	[TextArea]
+	public string Description;
+
+	[TextArea]
+	public string Location;
+
+	public bool IsDiscovered;
+}
---
+++ 2PD/Assets/Encyclo/Script/EncyclopediaUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EncyclopediaUI : MonoBehaviour {
+
+	public List<EncyclopediaItems> Items;
+
+	[SerializeField]
+	private Text ItemNameUI;
+
+	[SerializeField]
+	private Text ItemDescriptionUI;
+
+	[SerializeField]
+	private Text ItemLocationUI;
+
+	// Use this for initialization
+
+	public void DisplayUI(int index)
+	{
+		if (Items [index].IsDiscovered) 
+		{
+			ItemNameUI.text = Items [index].ItemName;
+			ItemDescriptionUI.text = Items [index].Description;
+			ItemLocationUI.text = Items [index].Location;
+		}
+	}
+}
