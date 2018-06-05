af0b11e66f0b7f34a9dad043b3c9c4aca7151753
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-05 20:29:16 +0800

    Added fade and start menu to build settings

---
+++ 2PD/Assets/Scripts/UI/Fade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour {
+
+	Animator animator;
+
+	public void Start()
+	{
+		animator = GetComponent<Animator>();
+		SceneManager.sceneLoaded += OnSceneLoaded;
+	}
+
+
+	public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		animator.SetTrigger ("Fade");
+	}
+}
