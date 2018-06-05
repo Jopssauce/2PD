7af55148d449b5ded7455f08361ce4abfdc36936
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-05 19:50:37 +0800

    Added resolution to floor 1

---
+++ 2PD/Assets/Scripts/Game Manager/ExitFloor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class ExitFloor : MonoBehaviour {
+	public int requiredPlayers;
+	public List<PlayerController> players;
+	public UnityEvent EventInRange;
+	public UnityEvent EventOutRange;
+	public string scene;
+
+	void Start()
+	{
+		EventInRange.AddListener(Exit);
+	}
+
+	void Exit()
+	{
+		if (players.Count == requiredPlayers)
+		{
+			SceneManager.LoadSceneAsync(scene);
+			SceneManager.UnloadSceneAsync("UI Scene");
+		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" && players.Any(player => player.playerID == col.gameObject.GetComponent<PlayerController>().playerID) == false)
+		{		
+			players.Add(col.gameObject.GetComponent<PlayerController>());
+			EventInRange.Invoke();
+
+		}
+
+
+	}
+	public void OnTriggerExit2D(Collider2D col)
+	{
+		if (col.gameObject.tag == "Player" && players.Any(player => player.playerID == col.gameObject.GetComponent<PlayerController>().playerID))
+		{
+			players.Remove(col.gameObject.GetComponent<PlayerController>());
+			EventOutRange.Invoke();
+
+		}
+
+	}
+}
