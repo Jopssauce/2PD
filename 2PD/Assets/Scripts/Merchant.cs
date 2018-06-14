f2d7d9c48c91e8e7558ae4dbc8ecbdab950d7446
Luis Gabriel D. Evaristo <>
2018-06-14 15:50:55 +0800

    added merchant script on UI Scene

---
+++ 2PD/Assets/Scripts/Merchant.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Merchant : MonoBehaviour {
+
+	private GameManager gameManager;
+	void Start () 
+	{
+		if (GameManager.instance != null)
+			gameManager = GameManager.instance;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
