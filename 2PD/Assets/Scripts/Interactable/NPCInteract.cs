88b20ec56ec068a0adad7ca49a9fe2ccae5b19fd
BENILDE\11531553 <johnjoseph.asuncion@benilde.edup.ph>
2018-06-18 10:37:06 +0800

    Added the interactable npc

---
+++ 2PD/Assets/Scripts/Interactable/NPCInteract.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteract : Interactable {
+
+
+	// Use this for initialization
+
+	public override void Start ()
+	{
+		base.Start ();
+	}
+
+	public override void Interact(PlayerController player)
+	{
+		EventInteract.Invoke();
+		Debug.Log ("Merchant");
+	}
+}
