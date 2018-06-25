5dc183d15186b09c2533be58a8c9f71e92dbdc33
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-25 18:21:38 +0800

    Added Amethyst Infuse

---
+++ 2PD/Assets/Scripts/Player/AmethystArrow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmethystArrow : BulletBehaviour {
+public float heal = 5;
+void OnTriggerEnter2D(Collider2D col)
+    {
+		if (col.gameObject.GetComponentInParent<PlayerStats>()) 
+		{
+			col.gameObject.GetComponentInParent<PlayerStats>().AddHp(heal);
+			col.gameObject.GetComponentInParent<Rigidbody2D>().AddForce(direction * knocbackForce, ForceMode2D.Force);
+		}
+		
+        Destroy(gameObject);
+    }
+}
