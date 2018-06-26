4bd5a9f8c2d7a3d650b5ec21a98cd3794fbaa443
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-26 23:08:05 +0800

    Added option to give skill to specific player

--- 2PD/Assets/Scripts/Interactable/InteractableAltar.cs
+++ 2PD/Assets/Scripts/Interactable/InteractableAltar.cs
@@ -6,10 +6,20 @@ using System.Linq;
 public class InteractableAltar : Interactable
 {
 	public BaseSkill skill;
+	public bool giveToSpecificPlayer;
+	public int playerID;
 	public override void Interact(PlayerController player)
 	{
 		SkillActor actor = player.GetComponent<SkillActor>();
-		if(actor.skills.Any(item => item.ID == skill.ID)) actor.AddSkill(skill);
+		if(actor.skills.Any(item => item.ID == skill.ID) && !giveToSpecificPlayer) actor.AddSkill(skill);
+		if(actor.skills.Any(item => item.ID == skill.ID) && giveToSpecificPlayer)
+		{
+			if (player.playerID == playerID)
+			{
+				actor.AddSkill(skill);
+			}
+			
+		}
 	}
 	
 }
