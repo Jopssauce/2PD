40d9490a7ee7f35a4ebf2c9d2e25fe3da3dcd395
Luis Gabriel D. Evaristo <>
2018-06-26 22:07:13 +0800

    Buttons will only activate upon end of splash screens.

--- 2PD/Assets/Scripts/TitleManager.cs
+++ 2PD/Assets/Scripts/TitleManager.cs
@@ -2,15 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour {
 
+	[SerializeField]
+	private Button StartButton;
 
+	[SerializeField]
+	private Button ExitButton;
+
+	private float Timer = 13.0f;
 
 	void Awake()
 	{
 		if(!isAudioOpen())
 			SceneManager.LoadSceneAsync("Audio", LoadSceneMode.Additive);
+		StartButton.interactable = false;
+		Debug.Log (StartButton.IsInteractable());
+		ExitButton.interactable = false;
+		Debug.Log (ExitButton.IsInteractable());
+		
 	}
 
 	bool isAudioOpen()
@@ -25,5 +37,20 @@ public class TitleManager : MonoBehaviour {
 			}
 		}
 		return false;
+	}
+
+	void Update()
+	{
+		Timer -= Time.deltaTime;
+		ButtonHandler ();
+	}
+
+	void ButtonHandler()
+	{
+		if (Timer <= 0.0f && !StartButton.IsInteractable() && !ExitButton.IsInteractable()) 
+		{
+			StartButton.interactable = true;
+			ExitButton.interactable = true;
+		}
 	}
 }
