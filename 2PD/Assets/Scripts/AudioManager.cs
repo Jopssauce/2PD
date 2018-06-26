751e2426a154ff4707e95ea183c92b2b5c24373a
Luis Gabriel D. Evaristo <>
2018-06-26 13:13:35 +0800

    Added the audio scripts and moved Title scene and Audio scene to Scenes folder, we have a problem with our Destructable script though

---
+++ 2PD/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour {
+
+	public static AudioManager instance;
+	public SoundFX[] Sounds;
+	public SoundFX[] Music;
+	// Use this for initialization
+	void Awake () 
+	{
+
+		if (instance == null) 
+		{
+			instance = this;	
+		}
+		else if (instance != this) 
+		{
+			Destroy (gameObject);
+		}
+
+		foreach (SoundFX s in Sounds)
+		{
+			s.Source = gameObject.AddComponent<AudioSource> ();
+			s.Source.clip = s.SoundEffect;
+			s.Source.loop = s.Loop;
+			// this is to help jops not get heart attack
+			s.Source.volume = s.Volume;
+		}
+
+		foreach (SoundFX s in Music)
+		{
+			s.Source = gameObject.AddComponent<AudioSource> ();
+			s.Source.clip = s.SoundEffect;
+			s.Source.loop = s.Loop;
+			// this is to help jops not get heart attack
+			s.Source.volume = s.Volume;
+		}
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	void OnDestroy()
+	{
+		instance = null;
+	}
+
+	void Start()
+	{
+		Play (MusicStrings.BGM);
+	}
+
+	public void Play (string name)
+	{
+		SoundFX s = Array.Find (Sounds, sound => sound.name == name);
+		s.Source.Play();
+	}
+
+	public void PlayMusic(string name)
+	{
+		SoundFX s = Array.Find (Music, sound => sound.name == name);
+		s.Source.Play();
+	}
+
+	public void PauseMusic(string name)
+	{
+		SoundFX s = Array.Find (Music, sound => sound.name == name);
+		s.Source.Pause();
+	}
+
+	public void StopMusic(string name)
+	{
+		SoundFX s = Array.Find (Music, sound => sound.name == name);
+		s.Source.Stop();
+	}
+}
---
+++ 2PD/Assets/Scripts/SoundFX.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SoundFX
+{
+
+	public string name;
+	public AudioClip SoundEffect;
+
+	[HideInInspector]
+	public AudioSource Source;
+
+	public bool Loop;
+
+	[Range(0.0f, 1.0f)]
+	public float Volume;
+}
+
+public class MusicStrings
+{
+	public static string BGM = "BGM";
+}
