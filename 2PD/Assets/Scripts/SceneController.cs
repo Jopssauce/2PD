0632a949f33e55428aa49a2cc9d9012e76fa73bf
Luis Gabriel D. Evaristo <>
2018-06-26 21:03:30 +0800

    Game now goes into floor 1 scene and added an exit application

--- 2PD/Assets/Scripts/SceneController.cs
+++ 2PD/Assets/Scripts/SceneController.cs
@@ -8,5 +8,13 @@ public class SceneController : MonoBehaviour
 	public void LoadScene(string name)
 	{
 		FindObjectOfType<AudioManager> ().Play (MusicStrings.SoundFx_Select);
+		SceneManager.LoadSceneAsync(name);
+	}
+
+	// Only Works on build
+	public void QuitApplication()
+	{
+		FindObjectOfType<AudioManager> ().Play (MusicStrings.SoundFx_Select);
+		Application.Quit ();
 	}
 }
