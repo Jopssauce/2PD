3ce41a64042596abad29209b9c49b294e623ef88
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-26 22:04:35 +0800

    Added list of pressure pads to wall animator

--- 2PD/Assets/Scripts/Animations/WallAnimator.cs
+++ 2PD/Assets/Scripts/Animations/WallAnimator.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.Linq;
 public class WallAnimator : MonoBehaviour {
 
 	Animator animator;
-	public Interactable pressurepad;
+	public List<Interactable> pressurepads;
+	
 
 	void Start()
 	{
@@ -14,11 +15,13 @@ public class WallAnimator : MonoBehaviour {
 
 	public void AnimLeft()
 	{
+		if(pressurepads.All(pad => pad.isInteracted == false)) return;
 		animator.SetTrigger("Left");
 	}
 
 	public void AnimRight()
 	{
+		if(pressurepads.All(pad => pad.isInteracted == false)) return;
 		animator.SetTrigger("Right");
 	}
 
