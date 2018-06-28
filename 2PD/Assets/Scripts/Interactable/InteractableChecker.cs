0f8f259bfbb52a464b1b2c9d9d0719654d2d525a
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-28 21:45:06 +0800

    Setting up interactable checker

---
+++ 2PD/Assets/Scripts/Interactable/InteractableChecker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Linq;
+
+public class InteractableChecker : MonoBehaviour 
+{
+	public List<Interactable> genericInteractable;
+
+	public bool areAllInteracted;
+	
+	public UnityEvent EventOnAllInteracted;
+
+	public bool checkAllInteracted()
+	{
+		if(genericInteractable.All(pad => pad.isInteracted == true)) 
+		{
+			areAllInteracted = true;
+			EventOnAllInteracted.Invoke();
+			return true;
+		}
+		areAllInteracted = false;
+		return false;
+	}
+}
