def793a2d887881a21323a9366096ef7a9055517
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-06-28 19:31:48 +0800

    Spawn Wave function and spawning coroutine

--- 2PD/Assets/Scripts/Spawner/Spawner.cs
+++ 2PD/Assets/Scripts/Spawner/Spawner.cs
@@ -12,6 +12,11 @@ public class Spawner : MonoBehaviour
 	public bool infiniteSpawning;
 	public float maxSpawn;
 	public float spawnInterval;
+	public float spawnWaves;
+	public float currentWave;
+	public float enemiesPerWave;
+	public Sprite on;
+	public Sprite off;
 	public List<SpawnEntities> prefabs;
 	public List<GameObject> allPrefabs;
 	public List<GameObject> spawnAreas;
@@ -24,7 +29,7 @@ public class Spawner : MonoBehaviour
 		EventOnDeActivate = new SpawnerEvents();
 	}
 
-	public void Spawn(GameObject prefab, Vector3 pos)
+	public void Spawn(GameObject prefab, Vector2 pos)
 	{
 		GameObject temp = Instantiate(prefab, pos, prefab.transform.rotation);
 		allPrefabs.Add(temp);
@@ -36,9 +41,52 @@ public class Spawner : MonoBehaviour
 		return new Vector2(Random.Range(bounds.min.x, bounds.max.x), Random.Range(bounds.min.y, bounds.max.y));
 	}
 
-	public IEnumerator Activate()
+	public void SpawnWave()
+	{
+		int spawnIndex = Random.Range(0, allPrefabs.Count - 1);
+		int prefabIndex = Random.Range(0, prefabs.Count - 1);
+		
+		for (int i = 0; i < enemiesPerWave; i++)
+		{
+			GameObject toSpawn = prefabs[prefabIndex].prefab.gameObject;
+			SpawnEntities toSpawnEntity = prefabs[prefabIndex];
+			GameObject spawnArea = spawnAreas[spawnIndex];
+
+			if (allPrefabs.Count - 1 < maxSpawn && infiniteSpawning)
+			{
+				if (toSpawnEntity.amountToSpawn < 
+				allPrefabs.Count(p => p.gameObject.GetComponent<PlayerController>().ID == toSpawnEntity.prefab.GetComponent<PlayerController>().ID))
+				{
+					Spawn(toSpawn, GetRandomPoint2D(spawnArea));
+				}
+			}
+			else if(allPrefabs.Count - 1 < maxSpawn && !infiniteSpawning && currentWave != spawnWaves)
+			{
+				if (toSpawnEntity.amountToSpawn < 
+				allPrefabs.Count(p => p.gameObject.GetComponent<PlayerController>().ID == toSpawnEntity.prefab.GetComponent<PlayerController>().ID))
+				{
+					Spawn(toSpawn, GetRandomPoint2D(spawnArea));
+				}
+				
+			}
+		}
+	}
+
+	public IEnumerator Spawning()
 	{
 		yield return new WaitForSeconds(spawnInterval);
+		currentWave++;
+
+	}
+
+	public void Activate()
+	{
+		StartCoroutine(Spawning());
+	}
+
+	public void DeActivate()
+	{
+		StopCoroutine(Spawning());
 	}
 	
 }
