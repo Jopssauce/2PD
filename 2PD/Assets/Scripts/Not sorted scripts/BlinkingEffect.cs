839544f38ea166b9c178f399f2e05c63f8712888
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-03 16:47:09 +0800

    Invulnerability timer and blinking effect started

---
+++ 2PD/Assets/Scripts/Not sorted scripts/BlinkingEffect.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BlinkingEffect : MonoBehaviour 
+{
+	public float blinkInterval = 0.1f;
+
+	public UnityEvent EventActivate;
+	public UnityEvent EventDeactivate;
+
+	public SpriteRenderer sr;
+
+	IEnumerator blink;
+
+	void Start()
+	{
+		EventActivate.AddListener(Activate);
+		EventDeactivate.AddListener(Deactivate);
+	}
+
+	public void Activate()
+	{
+		blink = Blinking();
+		StartCoroutine(blink);
+	}
+
+	public void Deactivate()
+	{
+		StopAllCoroutines();
+	}
+
+	public IEnumerator Blinking()
+	{
+		while (true)
+		{
+			sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0);
+			yield return new WaitForSeconds(blinkInterval);
+			sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 255);
+		}
+		
+	}
+	
+}
--- 2PD/Assets/Scripts/Player/Health.cs
+++ 2PD/Assets/Scripts/Player/Health.cs
@@ -5,17 +5,23 @@ using UnityEngine.Events;
 
 public class Health : MonoBehaviour {
 	public bool destroyOnHealthDepleted;
+	bool isInvulnerable = false;
+	bool isInvulnerabilityTimerStarted;
 	public float health;
 	public float maxHealth;
+	public float invulnerabilityTimer = 1;
 	public UnityEvent EventOnHealthChange;
 	public UnityEvent EventOnHealthDepleted;
 	public UnityEvent EventOnDestroy;
+	public UnityEvent EventInvincible;
+	public UnityEvent EventWasInvincible;
 
 	[Header("Damage Modifiers")]
 	public float globalModifier = 1;
 	public float fireModifier = 1;
 	public float lavaModifier = 1;
 
+	IEnumerator invulTimer;
 	void Awake()
 	{
 		health = maxHealth;
@@ -39,6 +45,12 @@ public class Health : MonoBehaviour {
 
 	public virtual void DeductHp(float amt, GameObject actor)
 	{
+
+		if(isInvulnerable)
+		{
+			return;
+		} 
+
 		if (amt <= 0) amt = 0;
 		//if(!isServer) return;
 		health -= amt;
@@ -51,6 +63,16 @@ public class Health : MonoBehaviour {
 		} 
 		Debug.Log("Hit");
 		EventOnHealthChange.Invoke();
+		if(!isInvulnerable)
+		{
+			if (!isInvulnerabilityTimerStarted)
+			{
+				isInvulnerable = true;
+				invulTimer = InvulnerabilityTimer();
+				StartCoroutine(invulTimer);
+			}
+
+		}
 	}
 
 	public virtual void AddHp(float amt)
@@ -65,6 +87,16 @@ public class Health : MonoBehaviour {
 	public void OnHealthDepeleted()
 	{
 		Destroy(this.gameObject);
+	}
+
+	public IEnumerator InvulnerabilityTimer()
+	{
+		EventInvincible.Invoke();
+		isInvulnerabilityTimerStarted = true;
+		yield return new WaitForSeconds(invulnerabilityTimer);
+		isInvulnerabilityTimerStarted = false;
+		isInvulnerable = false;
+		EventWasInvincible.Invoke();
 	}
 
 	void OnDestroy()
