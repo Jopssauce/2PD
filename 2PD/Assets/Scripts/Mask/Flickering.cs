391ec14042956b3873ee20d04e2c3b2948abe389
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-03 11:33:07 +0800

    For mask flickering effect

---
+++ 2PD/Assets/Scripts/Mask/Flickering.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flickering : MonoBehaviour {
+	public float flickInterval;
+	[Range(0.0f, 1.0f)]
+	public float flickSize;
+	IEnumerator flicker;
+	Vector3 prevScale;
+	Vector3 newScale;
+	void Start()
+	{
+		flicker = Flicker();
+		StartCoroutine(flicker);
+	}
+
+	public IEnumerator Flicker()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(flickInterval);
+			prevScale = transform.localScale;
+			newScale = new Vector3(transform.localScale.x + flickSize, transform.localScale.x + flickSize , 1);
+			transform.localScale = newScale;
+			yield return new WaitForSeconds(flickInterval);
+			transform.localScale = prevScale;
+		}
+		
+	}
+}
