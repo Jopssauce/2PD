1d3edeff4aeaa779e0ba5c3fd0b5dc7aa0e4f584
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-05 18:51:19 +0800

    Lava and Debuff

---
+++ 2PD/Assets/Scripts/Hazards/Lava.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+public class Lava : MonoBehaviour {
+	public BaseBuff buff;
+	
+	public void OnTriggerStay2D(Collider2D col)
+	{
+		if(buff == null) return;
+		BuffReceiver receiver = col.gameObject.GetComponent<BuffReceiver>();
+		if(receiver == null) return;
+		if (receiver.buffs.Any(p => p.buffType == buff.buffType) == true)
+		{
+			return;
+		}
+		if (receiver.buffs.Any(p => p.ID == buff.ID) == false)
+		{
+			receiver.AddBuff(buff);
+		}
+		
+	}
+}
---
+++ 2PD/Assets/Scripts/Skills and Buffs/Buffs/DebuffDamageOvertime.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebuffDamageOvertime : BaseBuff 
+{
+ 	public float effectInterval = 0.4f;
+	public float damage = 8;
+
+	public override void Activate(BuffReceiver receiver)
+    {
+       
+        PlayerController player = receiver.GetComponent<PlayerController>();
+        base.Activate(receiver);
+        if (isActivated == true)
+        {
+			StartCoroutine(OvertimeEffect(receiver));
+			StartCoroutine(Duration(receiver));
+            Debug.Log("Do" + this.name);
+        }
+         if (isActivated == false)
+        {
+			StopCoroutine(OvertimeEffect(receiver));
+			StopCoroutine(Duration(receiver));
+			receiver.RemoveBuff(this);
+            Debug.Log("Undo" + this.name);
+        }
+    }
+
+	public IEnumerator OvertimeEffect(BuffReceiver receiver)
+	{
+
+		while(true)
+		{
+			receiver.GetComponent<Health>().DeductHp(damage);
+			yield return new WaitForSeconds(effectInterval);		
+		}
+		
+	}
+
+	public IEnumerator Duration(BuffReceiver receiver)
+	{
+		yield return new WaitForSeconds(duration);
+		StopCoroutine(OvertimeEffect(receiver));
+		receiver.RemoveBuff(this);	
+	}
+
+}
