9b51bf8361c3232bf84583f7a19f076296acca04
Luis Gabriel D. Evaristo <>
2018-07-05 11:31:13 +0800

    Added a script for for holding the dialogue function

---
+++ 2PD/Assets/DialogueEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueEvent : MonoBehaviour
+{
+	UIManager uiManager;
+
+	void Start()
+	{
+		uiManager = UIManager.instance;
+	}
+
+	public void ActivateDialogue(string dialogue)
+	{
+		uiManager.CanvasUI.GetComponent<CanvasController>().dialogueBox.SetActive (true);
+		uiManager.CanvasUI.GetComponent<CanvasController>().dialogue.text = dialogue;
+		StartCoroutine (uiManager.CanvasUI.HideDialogue ());
+	}
+}
