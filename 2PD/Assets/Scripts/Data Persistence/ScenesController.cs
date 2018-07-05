b84767a1b594002e1741f1863de05307dd05d76e
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-05 13:46:20 +0800

    Starting on persistent scene

---
+++ 2PD/Assets/Scripts/Data Persistence/ScenesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesController : MonoBehaviour {
+
+	public Scene titleScene;
+	public void Awake()
+	{
+		if(!isSceneOpen("Title Scene")) SceneManager.LoadSceneAsync("Title Scene", LoadSceneMode.Additive);
+	}
+
+	
+	bool isSceneOpen(string name)
+	{
+		Scene UIscene = SceneManager.GetSceneByName(name);
+		for (int i = 0; i < SceneManager.sceneCount; i++)
+		{
+			if (SceneManager.GetSceneAt(i) == UIscene)
+			{
+				
+				return true;
+			}
+		}
+		return false;
+	}
+
+	
+}
