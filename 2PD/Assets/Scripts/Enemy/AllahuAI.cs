e12b62157bb59f1ba78e27dab5d4dc63eac6f3ed
BENILDE\11517215 <johnjoseph.asuncion@benilde.edu.ph>
2018-07-07 16:11:12 +0800

    Fixed lag caused by this

---
+++ 2PD/Assets/Scripts/Enemy/AllahuAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllahuAI : EnemyAI {
+
+	public override void ChasePlayer ()
+	{
+		//transform.position = Vector2.MoveTowards (transform.position, players[0].transform.position, moveSpeed * Time.deltaTime);
+		targetDirection = players[0].transform.position - enemyController.transform.position;
+		ResetDirection();
+		//transform.position += targetDirection * moveSpeed * Time.deltaTime;
+		enemyController.transform.position += targetDirection * moveSpeed * Time.deltaTime;
+	}
+
+}
