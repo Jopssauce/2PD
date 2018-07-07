43f880be1e072f1989690a602bc9eeaaf3f9578b
Luis Gabriel D. Evaristo <>
2018-07-07 10:03:00 +0800

    Added a new prefab of torch that lights up when the saphire is activated in floor 2, added new asset for unlit torch

---
+++ 2PD/Assets/Scripts/TorchAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchAnimation : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject LightSource;
+
+	private Animator am;
+
+	// Use this for initialization
+	void Start () 
+	{
+		am = GetComponent<Animator> ();
+	}
+
+	public void TorchActivated()
+	{
+		am.SetBool ("TorchOn", true);
+		LightSource.SetActive (true);
+	}
+}
