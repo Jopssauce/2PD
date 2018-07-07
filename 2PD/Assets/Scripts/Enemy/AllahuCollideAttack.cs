970e3357d6ad1ab4038af69e4f7bea8e9c3ac826
BENILDE\11517215 <johnjoseph.asuncion@benilde.edu.ph>
2018-07-07 15:52:38 +0800

    Allahu Akbar now works

---
+++ 2PD/Assets/Scripts/Enemy/AllahuCollideAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AllahuCollideAttack : EnemyCollideAttack {
+	public GameObject explosionPrefab;
+
+	public override void OnCollisionEnter2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{	
+			if(enemyAI.players.Count > 0) 
+			{
+				Instantiate (explosionPrefab, transform.position, explosionPrefab.transform.rotation);
+				Destroy (this.gameObject);
+			}		
+
+		}
+
+	}
+	public virtual void OnCollisionExit2D(Collision2D col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			
+		}
+	}
+
+}
