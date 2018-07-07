5d8ed7b7256e457da4b655b9d7b9c45c9b26958d
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-08 00:15:54 +0800

    Making bats lava proof

--- 2PD/Assets/Scripts/Hazards/Lava.cs
+++ 2PD/Assets/Scripts/Hazards/Lava.cs
@@ -9,10 +9,12 @@ public class Lava : MonoBehaviour {
 	{
 		if(buff == null) return;
 		BuffReceiver receiver = col.gameObject.GetComponent<BuffReceiver>();
+		if(receiver.GetComponent<PlayerController>().ID == 5) return;
 		if(receiver == null) return;
 
 		if (receiver.buffs.Any(p => p.ID == buff.ID) == false)
 		{
+			
 			receiver.AddBuff(buff);
 		}
 		
