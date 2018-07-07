7ac1134bda7065e7c6d8459d74c785aa8963f47d
Luis Gabriel D. Evaristo <>
2018-07-07 15:30:08 +0800

    Added a new script for handling the sounds to be put on events. Added a place holder sound when warrior is attacking.

---
+++ 2PD/Assets/Scripts/AudioEventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioEventHandler : MonoBehaviour {
+
+	private AudioManager audioManager;
+	// Use this for initialization
+	void Start () 
+	{
+		audioManager = FindObjectOfType<AudioManager> ();
+	}
+
+	public void PlaySFX(string name)
+	{
+		if(audioManager != null)
+			audioManager.Play (name);
+	}
+}
