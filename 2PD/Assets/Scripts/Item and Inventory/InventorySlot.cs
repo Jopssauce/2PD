74d20c5bfe8e544666a374d3fe4929daf5f511b7
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-08 16:50:57 +0800

    Added image to inventory slot

--- 2PD/Assets/Scripts/Item and Inventory/InventorySlot.cs
+++ 2PD/Assets/Scripts/Item and Inventory/InventorySlot.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventorySlot : MonoBehaviour 
 {
 	public int index;
 	public BaseItem item;
+	public Image image;
 
 	public void UseItem()
 	{
