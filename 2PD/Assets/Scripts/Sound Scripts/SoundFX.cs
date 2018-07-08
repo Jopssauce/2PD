f0d733ed8c02491a5cd7e08dfb01021926b36e44
Luis Gabriel D. Evaristo <>
2018-07-08 14:25:49 +0800

    Added sounds, when attacking for both players, when you damage the slime and bat, when you kill the slime and bat

--- 2PD/Assets/Scripts/Sound Scripts/SoundFX.cs
+++ 2PD/Assets/Scripts/Sound Scripts/SoundFX.cs
@@ -23,4 +23,8 @@ public class MusicStrings
 	public static string Music_BGM = "Title";
 	public static string SoundFx_Select = "Select";
 	public static string SoundFx_Hover = "Hover";
+	public static string SoundFx_ArrowShoot = "ArrowShoot";
+	public static string SoundFx_SwordSwing = "SwordSwing";
+	public static string SoundFx_SlimeAttacked = "SlimeAttacked";
+	public static string SoundFx_SlimeDeath = "SlimeDeath";
 }
