fecdd3e2d24931acf66a00b207a38fb0f2624602
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-08 10:33:23 +0800

    Gamemanager now loads persistent scene

--- 2PD/Assets/Scripts/Data Persistence/PersistentDataManager.cs
+++ 2PD/Assets/Scripts/Data Persistence/PersistentDataManager.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PersistentDataManager : MonoBehaviour {
-
+	public static PersistentDataManager instance;
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		instance = this;
 	}
 	
 	// Update is called once per frame
