b36c82273bc67327d007fa480c3b8148af801c8a
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-09 21:48:05 +0800

    Fixed music playing twice

--- 2PD/Assets/Scripts/UI/Retry.cs
+++ 2PD/Assets/Scripts/UI/Retry.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class Retry : MonoBehaviour {
 	PersistentDataManager persistentData;
+	public UnityEvent EventOnRetry;
 	void Start () 
 	{
 		if(persistentData == null)
@@ -22,5 +24,6 @@ public class Retry : MonoBehaviour {
 	public void RetryClick()
 	{
 		persistentData.StartChangeScene("Title Scene");
+		EventOnRetry.Invoke();
 	}
 }
