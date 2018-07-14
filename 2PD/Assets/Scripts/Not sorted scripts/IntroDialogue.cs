a2a1de2f2d449ae27ab2425c6c0ce38b3d233cb8
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-14 20:47:59 +0800

    Intro scene

---
+++ 2PD/Assets/Scripts/Not sorted scripts/IntroDialogue.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class IntroDialogue : MonoBehaviour
+{
+    public float timer = 5;
+    public GameObject dialogueBox;
+    public TextMeshProUGUI dialogueText;
+    public Queue<string> sentences;
+    [TextArea]
+    public List<string> dialogue;
+    IEnumerator startDialogue;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+        StartDialogue();
+    }
+
+    public void StartDialogue()
+    {
+        sentences.Clear();
+
+        foreach (string sentence in dialogue)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        startDialogue = DisplayNextSentence();
+        StartCoroutine(startDialogue);
+    }
+
+    public IEnumerator DisplayNextSentence()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timer);
+            dialogueBox.SetActive(true);
+            if (sentences.Count == 0)
+            {
+                EndDialogue();
+                yield break;
+            }
+
+            string sentence = sentences.Dequeue();
+			StartCoroutine(TypeText(sentence));
+        }
+
+    }
+	
+	public IEnumerator TypeText(string text)
+	{
+		string display = "";
+		foreach (char letter in text.ToCharArray())
+		{
+			//Use display to get dialogue letter per letter
+			display += letter;
+			 dialogueText.text = display;
+			yield return new WaitForSeconds(0f);
+		}
+	}
+
+    public void EndDialogue()
+    {
+        Debug.Log("End");
+    }
+
+}
---
+++ 2PD/Assets/Scripts/Not sorted scripts/IntroManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroManager : MonoBehaviour {
+	public List<PlayerController> players;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (var player in players)
+		{
+			player.EventOnDown.Invoke();
+			player.EventOnMove.Invoke();
+		}
+	}
+}
