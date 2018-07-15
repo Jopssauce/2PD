04cc91ed2ae6c2fd8319adf36d45649422fef063
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-07-15 23:04:56 +0800

    Diamond

---
+++ 2PD/Assets/Scripts/Skills and Buffs/Buffs/BuffDiamondInfuse.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffDiamondInfuse : BaseBuff {
+
+	public GameObject melee;
+    GameObject temp;
+    public override void Activate(BuffReceiver receiver)
+    {
+        base.Activate(receiver);
+        receiver.GetComponent<SpriteRenderer>().color = color;
+        ArcherCombat combat = receiver.GetComponent<ArcherCombat>();
+        if (isActivated == true)
+        {
+            if(!receiver.GetComponent<ArcherCombat>()) return;
+            temp = combat.objPrefab;
+            combat.objPrefab = melee;
+            Debug.Log("Do" + this.name);
+        }
+         if (isActivated == false)
+        {
+            combat.objPrefab = temp;
+            temp = null;
+            Debug.Log("Undo" + this.name);
+        }
+    }
+}
---
+++ 2PD/Assets/Scripts/Skills and Buffs/Skills/SkillDiamondInfuse.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class SkillDiamondInfuse : BaseSkill {
+
+	public BuffDiamondInfuse buff;
+	public override void Activate(GameObject actor, GameObject target)
+	{
+		base.Activate(actor, target);
+		if (target.GetComponent<BuffReceiver>())
+		{
+			BuffReceiver receiver = target.GetComponent<BuffReceiver>();
+			if (receiver.buffs.Any(item => item.buffType == buff.buffType) == true)
+			{
+				//BaseBuff temp = receiver.buffs.First(item => item.buffType == buff.buffType);
+				foreach (var item in receiver.buffs.ToArray())
+				{
+					if (item.buffType == buff.buffType)
+					{
+						item.Activate(receiver);
+						receiver.RemoveBuff(item);
+					}
+				}
+				//temp.Activate(receiver);			
+			}
+			if (receiver.buffs.Any(item => item.ID == buff.ID) == false)
+			{
+				receiver.AddBuff(buff);
+			}
+		}	
+	}	
+}
