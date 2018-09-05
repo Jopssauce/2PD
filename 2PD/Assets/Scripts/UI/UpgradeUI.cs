76098bff6dec1772bebbe6e72207647af5480e58
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-09-05 18:58:17 +0800

    - Added Upgrade Scene - Added Upgrade UI

---
+++ 2PD/Assets/Scripts/UI/UpgradeUI.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeUI : MonoBehaviour 
+{
+	public Text health1;
+	public Text damage1;
+	public PlayerController player1;
+	
+	// Update is called once per frame
+	void Update () {
+		health1.text = player1.GetComponent<Health>().maxHealth.ToString();
+		damage1.text = player1.GetComponent<PlayerCombat>().damage.ToString();
+	}
+}
