b496b77b705fe028b8da32105cbba6afa163b185
John Asuncion <johnjoseph.asuncion@benilde.edu.ph>
2018-09-05 17:53:19 +0800

    - Added IUpgradeable interface

---
+++ 2PD/Assets/Scripts/Upgrade/Upgrade.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour 
+{
+	interface IUpgradeable<T>
+	{
+		void Upgrade(int amt);
+		void Upgrade();
+	}
+	
+}
